0cba91b5c7d91f7b410d9f536e3ba87fe3f67247
dulcecanseco <20253ds119@utez.edu.mx>
2026-09-24 20:53:35 -0600

    Update Proyecto1DApplication.java

--- proyecto1D/src/main/java/mx/edu/utez/proyecto1D/Proyecto1DApplication.java
+++ proyecto1D/src/main/java/mx/edu/utez/proyecto1D/Proyecto1DApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@RequestMapping("/fizzbuzz&fibonacci")
+@RequestMapping("/fizzbuzzyfibonacci")
 public class Proyecto1DApplication {
 
 	public static void main(String[] args) {
